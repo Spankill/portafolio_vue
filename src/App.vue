e8c712436bf0783731e907249d94b45e6e126e09
ARUHIZA CARLOS <aruhiza87@gmail.com>
2026-05-22 00:39:19 -0400

    comiteando por las nubes

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Header from './components/Header.vue';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  AOS.init({
+    duration: 1000,
+    once: false,
+    offset:100,
+  })
+})
+
+</script>
+
+
+
+<template>
+  <div className="bg-[#111827] min-h-screen">
+
+    <Header />
+  </div>
+</template>
+ 
---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {ref} from 'vue';
+
+const isMenuOpen = ref(false);
+
+const menuItems = [
+      {name: 'Education', href:'#education'},
+      {name: 'Certificates', href:'#certificates'},
+      {name: 'About', href:'#about'},
+      {name: 'Skills', href:'#skills'},
+      {name: 'Projects', href:'#projects'},
+]
+const scrollToSection = (href) => {
+      isMenuOpen.value = false
+      const element = document.querySelector(href)
+      if(element){
+            element.scrollIntoView({
+                  behavior: 'smooth'
+            })
+      }
+}
+
+</script>
+
+
+
+<template lang="">
+      <div>
+            <header class="relative z-50 px-6 py-7">
+                  <div class="max-w-7xl mx-auto flex justify-between items-center">
+                        
+                  <div class="text-white text-3xl font-black cursor-pointer">
+                        PORTAFOLIO
+                        <span class="text-primary">.</span>
+                  </div>
+
+                  <nav></nav>
+
+                  </div>
+            </header>
+      </div>
+</template>
+
+
